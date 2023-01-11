875ada7dca84a5ac7f0d483874289e8364a8bd18
kirito2056 <hajin_2056@naver.com>
2023-01-12 01:31:54 +0900

    메인페이지 도착했으면 알림을 보내야지

--- backend/portfolio/src/main/java/controller/maincontroller.java
+++ backend/portfolio/src/main/java/controller/maincontroller.java
@@ -1,4 +1,12 @@
 package controller;
 
-public class maincontroller it{
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class maincontroller{
+    @RequestMapping("/")
+    private String mainpage() {
+        return ("메인페이지 도착");
+    }
 }
